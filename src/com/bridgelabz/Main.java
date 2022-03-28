dd0412e6a9dd29adc286ce09915411e5ed48c151
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-03-28 07:20:58 +0530

    Added: String Maximum

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -5,12 +5,13 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MaxTest test1 = new MaxTest();
-		Float[] inputArray = {1f,2f,3f};
-		test1.maxElement(inputArray);
-		Float[] inputArray1 = {1f,3f,2f};
+		String[] inputArray = {"Apple","Peach","Banana"};
+	    test1.maxElement(inputArray);
+		String[] inputArray1 = {"Peach","Banana","Apple"};
 		test1.maxElement(inputArray1);
-		Float[] inputArray2 = {3f,2f,1f};
+		String[] inputArray2 = {"Banana","Peach","Apple"};
 		test1.maxElement(inputArray2);
 	}
+	
 
 }
--- src/com/bridgelabz/MaxTest.java
+++ src/com/bridgelabz/MaxTest.java
@@ -3,7 +3,7 @@ package com.bridgelabz;
 public class MaxTest {
 	public void maxElement(Integer[] inputArray) {
 		for(int i=0; i<inputArray.length-1; i++) {
-			if(inputArray[i].compareTo(inputArray[i+1])==1) {
+			if(inputArray[i].compareTo(inputArray[i+1]) > 0) {
 				inputArray[i+1] = inputArray[i];
 			}
 		}
@@ -12,10 +12,19 @@ public class MaxTest {
 	
 	public void maxElement(Float[] inputArray) {
 		for(int i=0; i<inputArray.length-1; i++) {
-			if(inputArray[i].compareTo(inputArray[i+1])==1) {
+			if(inputArray[i].compareTo(inputArray[i+1]) > 0) {
 				inputArray[i+1] = inputArray[i];
 			}
 		}
 		System.out.println(inputArray[inputArray.length-1]);
+	}
+	
+	public void maxElement(String[] inputArray) {
+		for(int i=0; i<inputArray.length-1; i++) {
+			if(inputArray[i].compareTo(inputArray[i+1]) > 0) {
+				inputArray[i+1] = inputArray[i];
+			}
+		}
+		System.out.println(inputArray[inputArray.length - 1]);
 	}
 }
